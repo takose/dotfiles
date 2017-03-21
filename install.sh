97843b75cdc04e89b5fbc1337b61248c4b22a86a
Rina Takase <rina.m.takase@gmail.com>
2017-03-21 16:39:55 +0900

    .gitconfig

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/.vim ~/.config/nvim/
 ln -sf ~/.vimrc ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 git config --global core.editor

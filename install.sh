0251c61a49018fd74cc4650b5f03fe97da5e5de7
tako <rina.m.takase@gmail.com>
2018-12-07 19:45:18 +0900

    Setup Brewfile

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install rcmdnk/file/brew-file
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
@@ -11,6 +12,9 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/config/ ~/bin
 ln -sf ~/dotfiles/.hammerspoon/init.lua ~/.hammerspoon/init.lua
+ln -sf ~/dotfiles/Brewfile /.config/brewfile/Brewfile
 git config --global core.editor
 
+brew file install
 sh ./installer.sh ~/.cache/dein
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm

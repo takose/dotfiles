b31e5f1d4038552231b69f7ad54b26d9af9a85ca
tako <rina.m.takase@gmail.com>
2019-11-13 11:44:57 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,3 +19,6 @@ git config --global core.editor
 brew file install
 sh ./installer.sh ~/.cache/dein
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+defaults write com.google.Chrome NSUserKeyEquivalents -dict-add "Bookmark This Tab..." "@^$a" "Hide Google Chrome" "@^$h" "Select Next Tab" "@^f" "Select Previous Tab" "@^b"
+curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -o /usr/local/bin/diff-so-fancy

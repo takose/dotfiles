0e1bfca3468b22ca03b3938d8112034093a60234
tako <rina.m.takase@gmail.com>
2019-11-27 18:31:20 +0900

    Add global keybind

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -85,6 +85,7 @@ remapKey({'ctrl'}, 'p', keyCode('up'))
 -- テキスト編集
 remapKey({'ctrl'}, 'w', keyCode('x', {'cmd'}))
 remapKey({'ctrl'}, 'y', keyCode('v', {'cmd'}))
+remapKey({'ctrl'}, 'h', keyCode('delete'))
 
 -- コマンド
 remapKey({'ctrl'}, 's', keyCode('f', {'cmd'}))

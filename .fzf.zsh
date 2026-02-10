a928be59711ed9fd02e5dfde3e97239010090277
tako <rina.m.takase@gmail.com>
2026-02-10 23:51:03 +0900

    Fix path

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */opt/homebrew/bin/fzf* ]]; then
+  export PATH="${PATH:+${PATH}:}/opt/homebrew/bin/fzf"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+# [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+# source "/opt/homebrew/bin/fzf/shell/key-bindings.zsh"
